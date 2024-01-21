f928c10e87118ee4dab9480e4ddedb77d3163b66
Hania Flores <hanniaflores03@gmail.com>
2024-01-20 21:32:23 -0600

    Database

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -237,4 +237,20 @@ VALUES   (
     5
   );
 
+
+-- Query 6 Update all records in the inventory table to add "/vehicles" to the middle of the file
+-- path in the inv_image and inv_thumbnail columns.
+UPDATE 
+   public.inventory
+SET 
+   inv_image = REPLACE (
+  	inv_image,
+	'/images',
+	'/images/vehicles'
+   ),
+   inv_thumbnail = REPLACE (
+  	inv_thumbnail,
+	'/images',
+	'/images/vehicles'
+   );
     
