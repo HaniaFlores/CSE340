22b1a594d06a0351cc4d08a9bbd41531f0838530
Hania Flores <hanniaflores03@gmail.com>
2024-02-08 01:27:57 -0600

    CSS Details and Classification View

--- utilities/index.js
+++ utilities/index.js
@@ -68,13 +68,13 @@ Util.buildCarDetailsGrid = async function(data){
   <img class='car-img' src='${data.inv_image}' alt='Image of ${data.inv_make} ${data.inv_model} on CSE Motors'>
   <div class='details-box'>
   <h2 class="car-name">${data.inv_make} ${data.inv_model} Details</h2>
-  <p class="price">Price: ${new Intl.NumberFormat("en-US", {
+  <p class="price"><span>Price:</span> ${new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD"
   }).format(data.inv_price)}</p>
-  <p class="miles">Miles: ${new Intl.NumberFormat("en-US").format(data.inv_miles)}</p>
+  <p class="miles"><span>Miles:</span> ${new Intl.NumberFormat("en-US").format(data.inv_miles)}</p>
   <p class="description">${data.inv_description}</p>
-  <p class="color">Color: ${data.inv_color}</p>
+  <p class="color"><span>Color:</span> ${data.inv_color}</p>
   </div>`
 
   return grid
